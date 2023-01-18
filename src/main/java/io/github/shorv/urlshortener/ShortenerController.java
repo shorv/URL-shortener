b2c0e7609a6d6edc283572029857b972c4cd5cbd
shorv <kontakt.shorv@gmail.com>
2023-01-18 12:48:01 +0100

    change shortener mapping paths

--- src/main/java/io/github/shorv/urlshortener/ShortenerController.java
+++ src/main/java/io/github/shorv/urlshortener/ShortenerController.java
@@ -7,13 +7,12 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.net.URI;
 
 @RestController
-@RequestMapping(path = "/api/shortener")
 public class ShortenerController {
 
     private final ShortenerService shortenerService;
@@ -22,13 +21,14 @@ public class ShortenerController {
         this.shortenerService = shortenerService;
     }
 
-    @PostMapping()
+    @PostMapping("/api/v1")
+    @ResponseStatus(HttpStatus.CREATED)
     ShortenerResponse shortenUrl(@RequestBody ShortenerRequest request) {
         String hash = shortenerService.shortenUrl(request.url());
         return new ShortenerResponse(hash);
     }
 
-    @GetMapping("{hash}")
+    @GetMapping("/{hash}")
     ResponseEntity<HttpStatus> resolveUrl(@PathVariable String hash) throws HashUnknownException {
         String url = shortenerService.resolveUrl(hash);
 
--- src/main/java/io/github/shorv/urlshortener/ShortenerService.java
+++ src/main/java/io/github/shorv/urlshortener/ShortenerService.java
@@ -1,6 +1,5 @@
 package io.github.shorv.urlshortener;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
